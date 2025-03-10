cbe36bc251899c44dd0885533d57bafa33131813
Andrea Zago <info@andreazago.it>
2025-03-10 20:44:23 +0100

    feature: add header

--- app/dashboard/[projectId]/campaigns/[campaignId]/page.tsx
+++ app/dashboard/[projectId]/campaigns/[campaignId]/page.tsx
@@ -1,9 +1,29 @@
 import CampaignActions from "@/components/campaign-actions";
 import { getCampaignById } from "@/utils/campaign";
+import { Metadata } from "next";
 
 type CampaignPageProps = {
   params: Promise<{ projectId: string; campaignId: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: CampaignPageProps): Promise<Metadata> {
+  const { campaignId } = await params;
+  const campaign = await getCampaignById(campaignId);
+
+  if (!campaign) {
+    return {
+      title: "Campagna non trovata",
+      description: "La campagna richiesta non è stata trovata.",
+    };
+  }
+
+  return {
+    title: `${campaign.name} | NoxisFlow`,
+    description: "test",
+  };
+}
 
 const CampaignPage = async ({ params }: CampaignPageProps) => {
   const { projectId, campaignId } = await params;
